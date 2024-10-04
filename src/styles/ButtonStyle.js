03bcf8fb9149fd630f4df557fb2f438eebfde5d9
Pjh01 <estelle0102@naver.com>
2024-10-05 01:06:58 +0900

    Feat: 만들기 버튼

---
+++ src/styles/ButtonStyle.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import color from "../util/Color";
+
+export const Btn = styled.div`
+  width: 200px;
+  height: 45px;
+  background-color: ${color.black};
+  border-radius: 6px;
+  color: ${color.gray50};
+  text-align: center;
+  font-size: 14px;
+  line-height: 45px;
+  cursor: pointer;
+`;
