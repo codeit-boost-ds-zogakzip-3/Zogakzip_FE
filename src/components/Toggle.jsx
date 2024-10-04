11620526dbe6da5e0d3e56d1fd86b888afa84fae
Pjh01 <estelle0102@naver.com>
2024-10-05 01:40:31 +0900

    Feat: 토글 버튼

---
+++ src/components/Toggle.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import * as T from "../styles/ToggleStyle";
+
+const Toggle = () => {
+  const [isPublic, setIsPublic] = useState(false);
+
+  return (
+    <T.Toggle data-ispublic={isPublic} onClick={() => setIsPublic(!isPublic)}>
+      <T.ToggleButton data-ispublic={isPublic}></T.ToggleButton>
+    </T.Toggle>
+  );
+};
+
+export default Toggle;
---
+++ src/styles/ToggleStyle.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+import color from "../util/Color";
+
+export const Toggle = styled.div`
+  transition: all 0.4s ease-in-out;
+  position: relative;
+  width: 48px;
+  height: 24px;
+  cursor: pointer;
+  border-radius: 15px;
+  padding: 1.5px;
+  background-color: ${(props) =>
+    props["data-ispublic"] ? color.black : color.gray400};
+`;
+
+export const ToggleButton = styled.div`
+  transition: all 0.4s ease-in-out;
+  width: 50%;
+  height: 100%;
+  border-radius: 50%;
+  background-color: ${color.gray50};
+  transform: ${(props) =>
+    props["data-ispublic"] ? "translateX(24px)" : "translateX(0px)"};
+`;
