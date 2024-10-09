0114e5f568b6840dc8ea22168e7b38e112f595d9
Pjh01 <estelle0102@naver.com>
2024-10-09 17:31:27 +0900

    Feat: 검색바 수정

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,24 @@
 import * as S from "../styles/components/SearchStyle";
 import search from "../assets/search.svg";
+import { useState } from "react";
 
-const Search = ({ value, onChange, text }) => {
+const Search = ({ text, setSearchWord }) => {
+  const [keyword, setKeyword] = useState("");
+  const handleKeywordChange = (e) => setKeyword(e.target.value);
+  const handleKeyword = () => {
+    if (window.event.keyCode == 13) {
+      setSearchWord(keyword);
+    }
+  };
+
   return (
     <S.SearchContainer>
-      <S.SearchBar value={value} onChange={onChange} placeholder={text} />
+      <S.SearchBar
+        value={keyword}
+        onChange={handleKeywordChange}
+        placeholder={text}
+        onKeyUp={handleKeyword}
+      />
       <S.Icon src={search} alt="검색" />
     </S.SearchContainer>
   );
--- src/styles/components/SearchStyle.js
+++ src/styles/components/SearchStyle.js
@@ -3,10 +3,13 @@ import color from "../../util/Color";
 
 export const SearchContainer = styled.div`
   position: relative;
+  margin: 0 30px 0 10px;
+  flex: 1;
 `;
 
 export const SearchBar = styled.input`
   height: 45px;
+  width: calc(100% - 74px);
   background-color: ${color.gray100};
   border-radius: 6px;
   border: none;
